04584499371836d2b20c5ae2b86fecd259d15108
Balamurugan <msbalacse@gmail.com>
2023-06-19 21:40:38 +0530

    pattern added

--- Pattern/src/com/Pattern/Main.java
+++ Pattern/src/com/Pattern/Main.java
@@ -4,7 +4,7 @@ package com.Pattern;
 public class Main {
     public static void main(String[] args) {
         System.out.println();
-        pattern3(4);
+        pattern4(4);
     }
 
 //    Output of Pattern 1 Answer
@@ -14,7 +14,7 @@ public class Main {
 //    * * *
 //    * * * *
 
-    static void pattern1(int n){
+    static void pattern1(int n) {
         System.out.println("---------  Pattern 1  ---------");
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
@@ -25,15 +25,15 @@ public class Main {
     }
 
 
-//    Output of Pattern 2 Answer
+    //    Output of Pattern 2 Answer
 //    n = 4
 //    * * * *
 //    * * *
 //    * *
 //    *
-    static void pattern2(int n){
-        for(int i = 1; i <= n; i++ ){
-            for(int j = 1; j <= n - i + 1; j++ ){
+    static void pattern2(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
                 System.out.print("* ");
             }
             System.out.println();
@@ -50,11 +50,34 @@ public class Main {
 //     * *
 //     *
 
-    static void pattern3(int n){
+    static void pattern3(int n) {
         for (int i = 0; i <= 2 * n; i++) {
             int totalSpace = i > n ? 2 * n - i : i;
             for (int j = 0; j < totalSpace; j++) {
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+//     Output of Pattern 4 Answer
+//     n = 4
+//        *
+//       **
+//      ***
+//     ****
+//      ***
+//       **
+//        *
+    public static void pattern4(int n) {
+
+        for (int i = 0; i < 2 * n; i++) {
+            int totalSpace = n > i ? i : 2 * n - i;
+            for (int j = 0; j <= n - totalSpace; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= totalSpace - 1; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
