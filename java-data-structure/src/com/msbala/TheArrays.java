0753fafb8ddaf669888a8ac8a19a8b3a0a3e00cd
Balamurugan <msbalacse@gmail.com>
2023-08-06 19:27:33 +0530

    ds in java

---
+++ java-data-structure/src/com/msbala/TheArrays.java
@@ -0,0 +1,21 @@
+package com.msbala;
+
+import java.util.Arrays;
+
+public class TheArrays {
+    public static void main(String[] args) {
+        String[] colors = new String[5];
+        colors[0] = "purple";
+        colors[1] = "blue";
+
+        System.out.println(Arrays.toString(colors));
+
+        System.out.println(colors[0]);
+
+        for(String color: colors){
+            System.out.println(color);
+        }
+
+        Arrays.stream(colors).forEach(System.out::println);
+    }
+}
---
+++ java-data-structure/src/com/msbala/WorkingWithQueues.java
@@ -0,0 +1,37 @@
+package com.msbala;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Queue;
+
+public class WorkingWithQueues {
+    public static void main(String[] args) {
+      LinkedList<Person> linkedList = new LinkedList<>();
+      linkedList.add(new Person("alex",21));
+      linkedList.add(new Person("bonk",19));
+      linkedList.add(new Person("teo",34));
+        ListIterator<Person> personListIterator  = linkedList.listIterator();
+        while (personListIterator.hasNext()){
+            System.out.println(personListIterator.next());
+        }
+        System.out.println();
+        while (personListIterator.hasPrevious()){
+            System.out.println(personListIterator.previous());
+        }
+    }
+    private static void queues(){
+        Queue<Person> supermarket = new LinkedList<>();
+        supermarket.add(new Person("Alex",21));
+        supermarket.add(new Person("Maria,",34));
+        supermarket.add(new Person("Joona",16));
+
+        System.out.println(supermarket.size());
+        System.out.println(supermarket.peek());
+        System.out.println(supermarket.poll());
+        System.out.println(supermarket.size());
+        System.out.println(supermarket.peek());
+        System.out.println(supermarket.poll());
+        System.out.println(supermarket.size());
+    }
+    static record Person(String name, int age){}
+}
