ebefd4a187cae3e0bdbe968866b3888ade97eb26
Balamurugan <msbalacse@gmail.com>
2023-11-15 21:30:45 +0530

    excercise

--- java-practice/src/com/msbala/GettingStarted.java
+++ java-practice/src/com/msbala/GettingStarted.java
@@ -4,6 +4,21 @@ public class GettingStarted {
     public static void main(String[] args) {
         System.out.println("Java program has run successfully. ✨");
         compareTwoNumber(5,6);
+        toUpperCase("hello");
+
+        String name1 = "bala";
+        String name2 = "joshua";
+
+        if(name1.equals(name2)){
+            System.out.println("both are equal");
+        }else{
+            System.out.println("The two string are not same");
+        }
+    }
+
+    public static void toUpperCase(String str){
+        // Write a program that transform "amigoscode" to "Amigoscode"
+        System.out.println(str.toUpperCase());
     }
 
     public static void compareTwoNumber(int a,int b){
