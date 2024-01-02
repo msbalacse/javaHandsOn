cd34a971248b8d9721a316ac90012898336d22e7
Balamurugan <msbalacse@gmail.com>
2024-01-02 08:17:35 +0530

    user and booking package working

--- JavaMaster/src/com/msbala/Main.java
+++ JavaMaster/src/com/msbala/Main.java
@@ -1,5 +1,10 @@
 package com.msbala;
 
+import com.msbala.car.Brand;
+import com.msbala.car.Car;
+
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello java ✨");
---
+++ JavaMaster/src/com/msbala/booking/CarBookingDao.java
@@ -0,0 +1,43 @@
+package com.msbala.booking;
+
+import java.util.UUID;
+
+public class CarBookingDao {
+    private final static CarBooking[] carBookings;
+
+    static {
+        carBookings = new CarBooking[10];
+    }
+
+    public CarBooking[] getCarBookings() {
+        return carBookings;
+    }
+
+    public void book(CarBooking carBooking){
+        int nextFreeIndex = -1;
+
+        for (int i = 0; i < carBookings.length; i++) {
+            if(carBookings[i] == null){
+                nextFreeIndex = i;
+            }
+        }
+
+        if(nextFreeIndex > -1){
+            carBookings[nextFreeIndex] = carBooking;
+            return;
+        }
+
+        CarBooking[] biggerCarBookings = new CarBooking[carBookings.length + 10];
+
+        for (int i = 0; i < carBookings.length; i++) {
+            biggerCarBookings[i] = carBookings[i];
+        }
+
+        biggerCarBookings[carBookings.length] = carBooking;
+
+    }
+
+    public void cancelBooking(UUID id){
+
+    }
+}
