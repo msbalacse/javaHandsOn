6d9fd318a475b45321940a8756713ae235ce0467
Balamurugan <msbalacse@gmail.com>
2024-01-05 08:30:44 +0530

    reverse String

---
+++ Problems/src/com/gfg/ReverseString.java
@@ -0,0 +1,24 @@
+package com.gfg;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ReverseString {
+
+    public static String reverseString(String name){
+        int lenStr = name.length();
+        StringBuilder newStr = new StringBuilder("");
+        for (int i=lenStr - 1; i>=0; i--){
+            newStr.append(name.charAt(i));
+        }
+        return newStr.toString();
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String name = in.nextLine();
+
+//        System.out.println("The reverse string is "+ reverseString(name));
+        System.out.println(reverseString(name));
+    }
+}
